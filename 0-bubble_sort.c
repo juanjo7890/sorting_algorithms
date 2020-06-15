61c3e351854d8c2b05fff9a8a79732563bde4107
Juan Jose <1561@holbertonschool.com>
2020-06-15 19:20:05 +0000

    [add] other condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,9 +17,9 @@ void bubble_sort(int *array, size_t size)
 		return;
 	}
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i <= size - 1; i++)
 	{
-		for (j = 0; j < size - 2; j++)
+		for (j = 0; j <= size - 2; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
